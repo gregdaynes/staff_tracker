accf13b185594ce32df2339d22f0759f9e74a6b0
Gregory Daynes <gregdaynes@gmail.com>
2017-02-03 12:43:04 -0800

    Create & Read implemented
    navigation from root to staff list, staff list to each member and
    create, back navigation on create & view staff member.

--- app/controllers/staff_members_controller.rb
+++ app/controllers/staff_members_controller.rb
@@ -1,8 +1,29 @@
 class StaffMembersController < ApplicationController
+  def index
+    @staff_members = StaffMember.all
+  end
+
+  def show
+    @staff_member = StaffMember.find(params[:id])
+  end
+
   def new
+    @staff_member = StaffMember.new
   end
 
   def create
-    render plain: params[:staff_member].inspect
+    @staff_member = StaffMember.new(staff_member_params)
+
+    if @staff_member.save
+      redirect_to @staff_member
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def staff_member_params
+    params.require(:staff_member).permit(:first_name)
   end
 end
