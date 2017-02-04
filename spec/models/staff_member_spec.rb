fc7a1451d79d74a368c2dd2fc2d13fa9a74823e0
Gregory Daynes <gregdaynes@gmail.com>
2017-02-03 17:08:32 -0800

    Basic rspec for staff member

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it "is valid when first_name is 2 or more characters" do
+    subject.first_name = 'Al'
+    expect(subject).to be_valid
+  end
+
+  it "is not valid when first_name is 1 or less characters" do
+    subject.first_name = 'X'
+    expect(subject).to_not be_valid
+  end
 end
