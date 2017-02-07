ee64b0698a93aa2530531dfebb28887e997f0958
Gregory Daynes <gregdaynes@gmail.com>
2017-02-07 08:42:14 -0800

    Added language support to staff member
    I'm now realizing this is a terrible place for languages, and skills.
    Staff shouldn't discriminate about language or skills, those are things
    people poses.

--- spec/models/staff_member_language_spec.rb
+++ spec/models/staff_member_language_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe StaffMemberLanguage, type: :model do
-  subject { described_class.new( language: "Ruby" ) }
-
-  it "is valid when valid attributes" do
-    expect(subject).to be_valid
-  end
-
-  it "is not valid when language is empty" do
-    subject.language = nil
-    expect(subject).to_not be_valid
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
