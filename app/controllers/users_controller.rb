bd7aad161ade119e678b63b0b090f98d604fcc1a
Gregory Daynes <gregdaynes@gmail.com>
2017-02-08 09:21:33 -0800

    Working controller tests
    I've finally got controller tests working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,25 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    render json: @user
+
+    # if @user.save
+    #   redirect_to @users
+    # end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name)
+    end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 describe UsersController do
 
+
+  describe "GET #index" do
+    it "populates an array of users"
+    it "returns a json array of users"
+  end
+
+  describe "GET #show" do
+    it "assigns the requested user to @user" do
+      user = create(:user)
+      get :show, params: { id: user.id }
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it "returns a json object of @user" do
+      user = create(:user)
+      get :show, params: { id: user.id }
+      parsed_body = JSON.parse(response.body);
+      expect(parsed_body).to eq(JSON.parse(user.to_json))
+    end
+  end
+  
   describe "POST #create" do
     context "with valid attributes" do
       it "saves the new @user in the database"
@@ -13,16 +34,6 @@ describe UsersController do
       it "returns an json object with error message"
       it "has an error code of 400"
     end
-  end
-
-  describe "GET #index" do
-    it "populates an array of users"
-    it "returns a json array of users"
-  end
-
-  describe "GET #show" do
-    it "assigns the requested user to @user"
-    it "returns a json object of @user"
   end
 
   describe "UPDATE #edit" do
